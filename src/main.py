d5ac4e8a463b8784a2b66b9bfa12da5638d5fc45
Michael Taleff <mtaleff@utexas.edu>
2025-09-09 19:35:30 -0500

    Adding error message handler

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import sys
 from PySide6 import QtWidgets
 from view.main_window import Secan
+from dialogs.error_log_dialog import UncaughtHook
 
 
 def main():
     # Create QApplication instance
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     
     # Set application properties
     app.setApplicationName("Secan")
@@ -16,6 +17,9 @@ def main():
     window = Secan()
     window.resize(1000, 600)
     window.show()
+
+    # Starting a global instance of the error message box
+    qt_exception_hook = UncaughtHook()
     
     # Start the event loop
     sys.exit(app.exec())
